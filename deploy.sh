e625888e421ac499413f3a08b38e2fdb25956d24
VARSHINE <varshinet.22cse@kongu.edu>
2025-03-21 10:18:51 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
-    
+    docker login -u varsh1204 -p dckr_pat_I2_8Gn6JQMgzwWTNmFxAk1eVjSQ
+    docker tag test1 varsh1204/devops
+    docker push varsh1204/devops
